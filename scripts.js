e4a7c4dddbb909ebf095b0b69c0c6a80d9f020e4
mrkasore <mrkasore@gmail.com>
2023-11-12 23:35:56 +0300

    simple game done

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 const cells = document.querySelectorAll('.cell');
 let turn = 'x';
 let beforeTurn = turn;
-let setX = 0;
 
 cells.forEach(item => {
     item.addEventListener('click', move)
@@ -11,18 +10,13 @@ const board = createBoard();
 
 
 function move(e) {
-    
     if (board.arr[parseInt(e.target.getAttribute('data'))] == '-') {
-        console.log(e.target.getAttribute('data'));
         board.arr[parseInt(e.target.getAttribute('data'))] = turn;
         e.target.innerText = turn;
         beforeTurn = turn;
         (turn == 'x') ? turn = 'o' : turn = 'x';
-        console.log(checkWin(board.arr));
-    }
-    
-    //board.setMove(parseInt(e.target.getAttribute('data')), 'x');
-    
+        checkWin(board.arr);
+    } 
 }
 
 
@@ -55,14 +49,21 @@ function checkWin(board) {
         [2, 4, 6]
     ]
 
+    if(!board.includes('-')) {
+        console.log('Even');
+        return 'even';
+    }
+
     for (let i = 0; i < arrWin.length; i++) {
-        let setX = 0;
+        // count marks
+        let j = 0;
+        //
         for (let x = 0; x < 3; x++) {
             console.log(turn);
             if(board[arrWin[i][x]] == beforeTurn) {
-                setX++;
+                j++;
             }
-            if (setX > 2) {
+            if (j > 2) {
                 console.log(beforeTurn + ' ' + 'Win!');
                 return true;
             }
@@ -71,29 +72,5 @@ function checkWin(board) {
     }
     return false;
 }
-
-
-
-
-//Looping game untill win
-// while (!checkWin(board.arr)) {
-//     let choice = prompt('Type choice', '0, x');
-//     choice = choice.split(', ');
-//     board.setMove(parseInt(choice[0]), choice[1]);
-//     console.log(board.arr);
-//     console.log(checkWin(board.arr));
-// }
-
-
-
-
-
-//const talk = prompt('Type choice', '');
-//console.log(talk.split(', '));
-
-//board.setMove(0, 'x');
-//board.setMove(1, 'x');
-//board.setMove(2, 'x');
-
 
 
